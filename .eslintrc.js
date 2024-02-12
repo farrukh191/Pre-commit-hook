4fdd114ba32aacdc096d128ae211666beddeb6b1
@mfarrukh1 <mfarrukh@nisum.com>
2024-02-12 22:51:29 +0500

    dekho

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
+// .eslintrc.js
+
 module.exports = {
   extends: ["react-app", "react-app/jest"],
   rules: {
     "no-console": "error",
-    "no-unused-vars": "error",
     "no-unused-vars": ["error", { args: "none" }],
   },
 };
